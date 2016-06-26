38d850c556a9ee3ea61133f89e8eee497ff9489d
LeoNineStudios <LeoNineStudios@outlook.com>
2016-06-26 15:54:30 -0400

    UI Update

--- cgi-bin/footer.php
+++ cgi-bin/footer.php
@@ -3,5 +3,6 @@
 <script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js'></script>
 <script src='https://cdn.jsdelivr.net/jquery.ui.touch-punch/0.2.3/jquery.ui.touch-punch.min.js'></script>
 <script>window.jQuery || document.write("<script src='./js/vendor/jquery.min.js'><\/script>")</script>
+<script src="../js/leo.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 <script src="../js/ie10-viewport-bug-workaround.js"></script>
--- cgi-bin/navigation.php
+++ cgi-bin/navigation.php
@@ -8,31 +8,26 @@
         <span class='icon-bar'></span>
         <span class='icon-bar'></span>
       </button>
-      <a class='navbar-brand' href='../index.php'>PostersASAP.com</a>
+      <a class='navbar-brand' style='margin-left: calc(50% - 90px);display:none;' href='../index.php'>PostersASAP.com</a>
     </div>
     <div id='navbar' class='navbar-collapse collapse'>
-      <ul class='nav navbar-nav'>
-<?php
-        if (isset($_SESSION['userid']))
-        {
-          echo "<li class='dropdown'>
-            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Welcome " . $_SESSION['username'] . " <span class='caret'></span></a>
-            <ul class='dropdown-menu'>
-              <li><a href='../users/profile.php'>My Profile</a></li>
-              <li><a href='../users/logout.php''>Log Out</a></li>
-            </ul>
-          </li>";
-        }
-        else
-        {
-        echo "<li><a href='../users/signin.php'>Sign In</a></li>";
-        }
-?>
-      </ul>
       <ul class='nav navbar-nav navbar-right'>
-          <li><a href='../products/posters.php' style='background-color: #5cb85c'><span style='color:white'>Posters</span></a></li>
-          <li><a href='../products/banners.php' style='background-color: #5bc0de'><span style='color:white'>Banners</span></a></li>
-          <li><a href='../products/vinyl.php' style='background-color: #F89406'><span style='color:white'>Vinyl</span></a></li>
+<?php
+            if (isset($_SESSION['userid']))
+            {
+              echo "<li class='dropdown'>
+                <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Welcome " . $_SESSION['username'] . " <span class='caret'></span></a>
+                <ul class='dropdown-menu'>
+                  <li><a href='../users/profile.php'>My Profile</a></li>
+                  <li><a href='../users/logout.php''>Log Out</a></li>
+                </ul>
+              </li>";
+            }
+            else
+            {
+            echo "<li><a href='../users/signin.php'>Sign In</a></li>";
+            }
+?>
           <li><a href='../checkout/cart.php'><span class='glyphicon glyphicon-shopping-cart' style='color:white'></span></a></li>
       </ul>
     </div><!--/.nav-collapse -->
